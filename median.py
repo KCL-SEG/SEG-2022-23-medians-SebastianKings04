f25748f9c57f16c4f75af3cb75fdde50113df5c7
Sebastian Habram <sebastian.habram@kcl.ac.uk>
2023-09-26 16:44:56 +0100

    Complete Solution

--- median.py
+++ median.py
@@ -9,4 +9,21 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+switched = True
+while switched:
+    switched = False
+    for i in range(0, len(numbers)-1):
+        if numbers[i] > numbers[i+1]:
+            temp = numbers[i+1]
+            numbers[i+1] = numbers[i]
+            numbers[i] = temp
+            switched = True
+
+if len(numbers)%2 != 0: 
+    print(f"The median of this list is {numbers[(len(numbers)//2)]}")
+else:
+    a = float(len(numbers))/2
+    average = (numbers[int(a-0.5)]+numbers[int(a+0.5)])/2
+    print(f"The median of this list is {average}")
+    
